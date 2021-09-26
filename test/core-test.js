2ee28fd147c9bb1d7fe7c660625b2fb25a247d3b
RedRocket94 <Oliver.duedam@gmail.com>
2021-09-26 03:40:22 +0200

    feat: added simple AccessControl tests

--- test/core-test.js
+++ test/core-test.js
@@ -9,8 +9,13 @@ describe("EncodeDAOCore", function () {
   let EncodeDAOCore;
   let encodeDAOCore;
 
-  // Define the zero/burn address for future use
-  const zeroAddress = '0x0000000000000000000000000000000000000000';
+  // Defines the zero/burn address for future use
+  const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+  // Defines the default admin role assigned to owner on initialization
+  const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000';
+  const HASHED_MODERATOR_ROLE = keccak256("MODERATOR_ROLE");
+
 
   // Deploy contract before each test case
   beforeEach(async function () {
@@ -23,4 +28,18 @@ describe("EncodeDAOCore", function () {
     await encodeDAOCore.deployed();
   });
 
+  it("should set owner role to DEFAULT_ADMIN_ROLE", async function () {
+    expect(await encodeDAOCore.hasRole(DEFAULT_ADMIN_ROLE, owner.address)).to.equal(true);
+  });
+
+  it("should set addr1 role to MODERATOR_ROLE", async function () {
+    // Connects to contract with owner address and grants role to address of addr1
+    await encodeDAOCore.connect(owner).grantRole(HASHED_MODERATOR_ROLE, addr1.address);
+    expect(await encodeDAOCore.hasRole(HASHED_MODERATOR_ROLE, addr1.address)).to.equal(true);
+  });
 });
+
+// Short method to simplify keccak256 hashing
+function keccak256(strToHash) {
+  return ethers.utils.keccak256(ethers.utils.toUtf8Bytes(strToHash));
+}
