f142189f798d73316901c9e11a2103450ec9cdd4
Martin <martinloesethjensen@gmail.com>
2021-09-30 14:36:02 +0200

    feat: added natspec comments

--- contracts/EncodeDAOCore.sol
+++ contracts/EncodeDAOCore.sol
@@ -11,28 +11,29 @@ contract EncodeDAOCore is ERC721URIStorage, AccessControl {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    // Propose issue by name and funding
+    /// Propose issue by name and funding
     function proposeIssue(string memory name, uint256 fundingMinimum) public {}
 
-    // Vote on issue by passing issueId
+    /// Vote on issue by passing issueId
+    /// @notice Vote on issue with issue id: `issueId`
     function voteIssue(uint256 issueId) public {}
 
-    // Withdraw (everything) from failed issue
+    /// Withdraw (everything) from failed issue
     function withdrawFromFailedIssue() public {}
 
-    // Deposit funds to contract for continuous voting rights
+    /// Deposit funds to contract for continuous voting rights
     function deposit() public payable {}
 
-    // Get a list of apartment ids owned by yourself
+    /// Get a list of apartment ids owned by yourself
     function getApartmentList() public view {}
 
-    // Get the details of an apartment you own
+    /// Get the details of an apartment you own.
     function getApartmentDetails(uint256 apartmentId) public view {}
 
-    // Get a list of passed issues
+    /// Get a list of passed issues
     function getPassedIssues() public view {}
 
-    // IGNORE - Required to override in impl as both ERC721 and AccessControl define this
+    /// @dev IGNORE - Required to override in impl as both ERC721 and AccessControl define this
     function supportsInterface(bytes4 interfaceId)
         public
         view
